e67629933be7e207729d931e1f0e85d4f4e78fe3
Aniket Kadam <anikadamg@gmail.com>
2018-09-07 15:42:15 +0530

    Formatting changes

--- src/components/Dependency.js
+++ src/components/Dependency.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
 export default function Dependency({ dependency, ostype }) {
-  return <div>
-  Hi I'm a dependency called {dependency.name}
-  Get {dependency[ostype]}
-  </div>;
+  return (
+    <div>
+      Hi I'm a dependency called {dependency.name}
+      Get {dependency[ostype]}
+    </div>
+  );
 }
